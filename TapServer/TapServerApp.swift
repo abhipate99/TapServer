ba47fa0222df6a3e60d586973418eade08b2763a
Abhijeet Pate <abhipate99@gmail.com>
2025-07-11 21:19:13 +0530

    Linking local project to git repo.

---
+++ TapServer/TapServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapServerApp.swift
+//  TapServer
+//
+//  Created by Abhijeet Pate on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TapServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TapServerTests/TapServerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapServerTests.swift
+//  TapServerTests
+//
+//  Created by Abhijeet Pate on 11/07/25.
+//
+
+import Testing
+@testable import TapServer
+
+struct TapServerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
